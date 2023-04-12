62f26489f0dd11e7f9c0ec8c3b23dae639316c67
Lucia Ferencik <lferencikova@gmail.com>
2023-04-12 07:53:55 +0000

    Ready to deploy to Heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
 from pathlib import Path
+import os
+import dj_database_url
+import env
 import typing
 
-if os.path.exists("env.py"):
+if os.path.isfile("env.py"):
     import env
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -79,13 +81,16 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
